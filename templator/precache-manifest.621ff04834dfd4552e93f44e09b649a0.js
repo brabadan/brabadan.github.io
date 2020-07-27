9e42c43b61b64890f6d255d37466ade131081ebe
Rabadan Bakhmudov <brabadan@yandex.ru>
2020-07-28 00:28:16 +0300

    update Templator .

---
+++ templator/precache-manifest.621ff04834dfd4552e93f44e09b649a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ba9ba5a481bf70252f8f028abd730ff",
+    "url": "./index.html"
+  },
+  {
+    "revision": "159b4ce9f7b476262b31",
+    "url": "./static/js/2.32b08531.chunk.js"
+  },
+  {
+    "revision": "c8008c2db7a41de461fac719e4585e96",
+    "url": "./static/js/2.32b08531.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af77072ec98da2c8d9b0",
+    "url": "./static/js/main.1089dcb9.chunk.js"
+  },
+  {
+    "revision": "a4c0d3c693dae6994590",
+    "url": "./static/js/runtime-main.9bdf2b3d.js"
+  }
+]);
