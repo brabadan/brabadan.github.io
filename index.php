ea6d68fc29c22c1b0d628329746f368d7c47692d
Rabadan Bakhmudov <brabadan@yandex.ru>
2020-07-27 22:00:53 +0300

    add Portfolio repo

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once "studio/vendor/autoload.php";
-
-use \Brabadan\Test\SkeletonClass;
-
-$class = new SkeletonClass();
-
-echo $class->echoPhrase("Оно работает! - v1.0.1 commit 2!");
