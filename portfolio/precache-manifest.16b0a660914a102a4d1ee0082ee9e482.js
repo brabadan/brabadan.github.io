755cfed427feec8b6e3dd390d632734b00369d76
Rabadan Bakhmudov <brabadan@yandex.ru>
2020-07-27 22:04:18 +0300

    add Portfolio build

---
+++ portfolio/precache-manifest.16b0a660914a102a4d1ee0082ee9e482.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a36cab965e69b3394a7c1568bbd0d8ee",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f0df7a463f4dc9bf5607",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bf79e0fc48d59fd198f7",
+    "url": "./static/js/2.4c736102.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.4c736102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0df7a463f4dc9bf5607",
+    "url": "./static/js/main.f225874d.chunk.js"
+  },
+  {
+    "revision": "610a38b7b5a7cb7b2d5e",
+    "url": "./static/js/runtime-main.916853f0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
